d142694faa565e23461bc8e7d8083cf094817961
alexhunterpro <alexhunterpro0@gmail.com>
2026-03-11 03:23:40 +0600

    Update videodownload.py

--- videodownload.py
+++ videodownload.py
@@ -1,3 +1,7 @@
-# Download.py
-def main():
-    print("Video Download Tool Running...")
+import Download
+
+if __name__ == "__main__":
+    try:
+        Download.main()
+    except AttributeError:
+        print("Module loaded but main() function not found.")
